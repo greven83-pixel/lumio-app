569cf354b895c4e3ce46a6afc13ec65f4a9f326d
greven83-pixel <alessandro.sciacco@gmail.com>
2026-03-15 11:12:17 +0800

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import LumioBassano from './App.jsx'
+import LumioBassano from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
